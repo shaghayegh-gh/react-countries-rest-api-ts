3b3cbd686b0f58173faad5e423547b109d6746e9
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-04 21:48:02 +0330

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routes } from "./router/route-map";
-import "./index.css"
+import "./index.css";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="bg-gray-50 dark:bg-gray-700 dark:text-gray-50 min-h-screen  font-sans">
       <BrowserRouter>
+        <Header />
         <Routes>
           {routes.map((route, index) => (
             <Route key={index} {...route} />
