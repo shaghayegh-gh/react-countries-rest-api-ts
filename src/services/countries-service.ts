23f5813058f4afff6411f18004e492439c8c3a9c
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-02-04 21:43:40 +0330

    Add filter services

--- src/services/countries-service.ts
+++ src/services/countries-service.ts
@@ -1,5 +1,12 @@
 import { api } from "../setup/api";
 import { getRequest } from "../setup/axios-config";
+import { CountriesType } from "../setup/global-types";
 
 export const getCountries = async () =>
   await getRequest(api.countries).then((res) => res);
+
+export const getCountriesByRegion = async (region: string) =>
+  await getRequest(api.region(region)).then((res: CountriesType) => res);
+
+export const getCountryByName = async (name: string) =>
+  await getRequest(api.county(name)).then((res: CountriesType) => res);
--- src/setup/api.ts
+++ src/setup/api.ts
@@ -1,3 +1,5 @@
 export const api = {
   countries: "/all",
+  region: (region:string) => `/region/${region}`,
+  county: (name:string) => `/name/${name}`,
 };
